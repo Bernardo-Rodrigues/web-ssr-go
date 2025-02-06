09005a5028f6f2b2fe8e48ba2f44a89618363dda
Bernardo Rodrigues <bernardo.rodrigues06@unifei.edu.br>
2025-02-06 11:51:28 -0300

    feat: add dynamic content with htmx

--- main.go
+++ main.go
@@ -11,12 +11,20 @@ import (
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	err := templates.Home().Render(context.Background(), w)
 	if err != nil {
-		http.Error(w, "Erro ao renderizar template", http.StatusInternalServerError)
+		http.Error(w, "Error rendering template", http.StatusInternalServerError)
+	}
+}
+
+func loadMessageHandler(w http.ResponseWriter, r *http.Request) {
+	err := templates.Message().Render(context.Background(), w)
+	if err != nil {
+		http.Error(w, "Error loading message", http.StatusInternalServerError)
 	}
 }
 
 func main() {
 	http.HandleFunc("/", homeHandler)
-	fmt.Println("Servidor rodando em http://localhost:8080")
+	http.HandleFunc("/load-message", loadMessageHandler)
+	fmt.Println("Server running at http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- templates/message_templ.go
+++ templates/message_templ.go
@@ -8,7 +8,7 @@ package templates
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
-func Home() templ.Component {
+func Message() templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
 		if templ_7745c5c3_CtxErr := ctx.Err(); templ_7745c5c3_CtxErr != nil {
@@ -29,7 +29,7 @@ func Home() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html lang=\"pt\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Home Page</title><script src=\"https://unpkg.com/htmx.org@1.9.6\"></script></head><body><h1>Welcome to Web SSR Go!</h1><button hx-get=\"/load-message\" hx-target=\"#message\" hx-swap=\"innerHTML\">Load Message</button><div id=\"message\"></div></body></html>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<p>This is a message dynamically loaded with HTMX!</p>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
